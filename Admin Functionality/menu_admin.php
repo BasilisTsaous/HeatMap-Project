fc8dfb06663937fcad848ea42106d0a7dc63bcba
BasilisTsaous <76739991+BasilisTsaous@users.noreply.github.com>
2025-01-17 19:14:37 +0200

    Update menu_admin.php

--- Admin Functionality/menu_admin.php
+++ Admin Functionality/menu_admin.php
@@ -1,19 +1,19 @@
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="admin.php">HeatMap Location History (2019-2020) <font size="2"> (Διαχειριστής)</font></a>
+      <a class="navbar-brand" href="admin.php">HeatMap Location History (2019-2020) <font size="2"> (Administrator)</font></a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
 		<form class='navbar-form navbar-right' method = 'post' action = "logout.php">
 			<div class='form-group' >
-				<label id = "synd" style = "color:white">Έχετε συνδεθεί ως &nbsp;&nbsp;<font color="lightseagreen"><?php echo $_SESSION["first"]." ".$_SESSION["last"]; ?></font> &nbsp;&nbsp;&nbsp;</label>
+				<label id = "synd" style = "color:white">You are signed in as &nbsp;&nbsp;<font color="lightseagreen"><?php echo $_SESSION["first"]." ".$_SESSION["last"]; ?></font> &nbsp;&nbsp;&nbsp;</label>
 			
 			<ul class="navbar-nav">
                 <li class="nav-item">
-                  <a class="nav-link" onclick="scroll('map_elements');" href="admin.php#map_elements">Απεικόνιση Στοιχείων Χάρτη</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <a class="nav-link" onclick="scroll('map_elements');" href="admin.php#map_elements">Map Elements Display</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" onclick="scroll('dashboard');" href="admin.php#dashboard">Απεικόνιση ΒΔ</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <a class="nav-link" onclick="scroll('dashboard');" href="admin.php#dashboard">Display DataBase </a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 </li>
             </ul>
 			
